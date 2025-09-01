6eb18008c51e6c44f5bb1b3d466890f1d0296a81
gloriak9 <asglor@gmail.com>
2025-09-01 20:38:36 +0200

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const pollRoutes = require('./routes/pollRoutes.js');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({ origin: 'https://munichsafewalk.netlify.app' }));
 app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URI, {
